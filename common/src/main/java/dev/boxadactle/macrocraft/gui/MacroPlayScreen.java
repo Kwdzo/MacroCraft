31efc1265a8caf3e45a2e769358678a795442931
Kwdzo <kullterooo@gmail.com>
2025-02-06 20:05:27 +0100

    Update MacroPlayScreen.java

--- common/src/main/java/dev/boxadactle/macrocraft/gui/MacroPlayScreen.java
+++ common/src/main/java/dev/boxadactle/macrocraft/gui/MacroPlayScreen.java
@@ -73,7 +73,7 @@ public class MacroPlayScreen extends BOptionScreen {
                 if (!flag) {
                     return;
                 }
-                MacroState.LOADED_MACRO.repetitions = 12;
+                MacroState.LOADED_MACRO.repetitionsDone = 0;
 
                 button.setMessage(Component.translatable("screen.macrocraft.play.pause"));
 
