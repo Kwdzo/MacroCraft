372a5b9a381aa6ec0cbb25ed7f24c021c61430e8
Kwdzo <kullterooo@gmail.com>
2025-02-07 08:33:17 +0100

    Update MacroSerializer.java

--- common/src/main/java/dev/boxadactle/macrocraft/json/MacroSerializer.java
+++ common/src/main/java/dev/boxadactle/macrocraft/json/MacroSerializer.java
@@ -27,6 +27,8 @@ public class MacroSerializer implements JsonSerializer<Macro> {
         JsonObject object = new JsonObject();
         object.addProperty("duration", macro.duration);
         object.addProperty("repetitions", macro.duration);
+        object.addProperty("startingXRot", macro.startingXRot);
+        object.addProperty("startingXRot", macro.startingXRot);
         object.add("actions", array);
 
         return object;
