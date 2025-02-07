2abbcc875682084628158cfd070d7e0da5f47005
Kwdzo <kullterooo@gmail.com>
2025-02-07 07:52:10 +0100

    Update and rename SetRotMixin.java to EntityInvoker.java

--- common/src/main/java/dev/boxadactle/macrocraft/listeners/EntityInvoker.java
+++ common/src/main/java/dev/boxadactle/macrocraft/listeners/EntityInvoker.java
@@ -1,10 +1,10 @@
 package dev.boxadactle.macrocraft.listeners;
 
-import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
+import net.minecraft.world.entity.Entity;
 
 @Mixin(Entity.class)
 public interface EntityInvoker {
